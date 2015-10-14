d3230532a78396187fd545a571d62f0414118ee2
zhy2002 <hz141@uowmail.edu.au>
2015-10-14 11:34:28 +1100

    Added a new test.

--- src/test/java/zhy2002/aspectjexamples/test/ThreadTest.java
+++ src/test/java/zhy2002/aspectjexamples/test/ThreadTest.java
@@ -2,7 +2,17 @@ package zhy2002.aspectjexamples.test;
 
 
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
 import static org.testng.Assert.*;
@@ -103,7 +113,8 @@ public class ThreadTest {
 
             assertFalse(Thread.currentThread().isInterrupted());
 
-            while (!Thread.currentThread().isInterrupted());  // The <i>interrupted status</i> of the thread is unaffected by this method.
+            while (!Thread.currentThread().isInterrupted())
+                ;  // The <i>interrupted status</i> of the thread is unaffected by this method.
 
             assertTrue(Thread.interrupted());
 
@@ -119,15 +130,15 @@ public class ThreadTest {
 
 
     @Test
-    public void joinTest() throws InterruptedException{
+    public void joinTest() throws InterruptedException {
 
         final String[] flag = new String[1];
 
-        Thread t1 = new Thread(()->{
+        Thread t1 = new Thread(() -> {
             try {
                 Thread.sleep(100);
                 flag[0] = "done";
-            }catch (InterruptedException ex){
+            } catch (InterruptedException ex) {
                 throw new RuntimeException(ex);
             }
 
@@ -141,23 +152,23 @@ public class ThreadTest {
     private static class SynchronizedCounter {
         private int count = 0;
 
-        public SynchronizedCounter (){
+        public SynchronizedCounter() {
             this(0);
         }
 
-        public SynchronizedCounter (int count){
+        public SynchronizedCounter(int count) {
             this.count = count;
         }
 
-        public synchronized void increase(){
+        public synchronized void increase() {
             count++;
         }
 
-        public synchronized void decrease(){
+        public synchronized void decrease() {
             count--;
         }
 
-        public synchronized int getCount(){
+        public synchronized int getCount() {
             return count;
         }
     }
@@ -165,32 +176,33 @@ public class ThreadTest {
     /**
      * it is not possible for two invocations of synchronized methods on the same object to interleave.
      * when a synchronized method exits, it automatically establishes a happens-before relationship with any subsequent invocation of a synchronized method for the same object. This guarantees that changes to the state of the object are visible to all threads.
+     *
      * @throws InterruptedException
      */
     @Test
-    public void synchronizedCounterTest() throws InterruptedException{
+    public void synchronizedCounterTest() throws InterruptedException {
         final CountDownLatch countDownLatch = new CountDownLatch(1);
-        final SynchronizedCounter  counter = new SynchronizedCounter ();
-        Thread t1 = new Thread(()->{
+        final SynchronizedCounter counter = new SynchronizedCounter();
+        Thread t1 = new Thread(() -> {
             try {
                 countDownLatch.await();
-                for(int i=0; i<1000;i++){
+                for (int i = 0; i < 1000; i++) {
                     counter.increase();
                 }
-            }catch (InterruptedException ex){
+            } catch (InterruptedException ex) {
                 throw new RuntimeException(ex);
             }
         });
 
-        Thread t2 = new Thread(()->{
+        Thread t2 = new Thread(() -> {
             try {
 
                 countDownLatch.await();
-                for(int i=0; i<1000;i++){
+                for (int i = 0; i < 1000; i++) {
                     counter.decrease();
                 }
 
-            }catch (InterruptedException ex){
+            } catch (InterruptedException ex) {
                 throw new RuntimeException(ex);
             }
         });
@@ -205,5 +217,139 @@ public class ThreadTest {
 
     }
 
-    //resume here: https://docs.oracle.com/javase/tutorial/essential/concurrency/locksync.html
+    @Test
+    public void testWaitNotify() throws InterruptedException {
+
+        final Object waitHandle = new Object();
+        final List<String> messageSequence = Collections.synchronizedList(new ArrayList<>());
+
+        Thread thread1 = new Thread(() -> {
+
+            synchronized (waitHandle) {
+                messageSequence.add("Thread1 before wait");
+                try {
+                    waitHandle.wait(); //release lock and try to reacquire after being notified
+                    messageSequence.add("Thread1 after wait");
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(50);
+
+                synchronized (waitHandle) {
+                    messageSequence.add("Thread2 before notify");
+                    waitHandle.notify(); //about to release lock
+                    messageSequence.add("Thread2 after notify");
+                }
+
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        assertEquals(messageSequence, Arrays.asList("Thread1 before wait", "Thread2 before notify", "Thread2 after notify", "Thread1 after wait"));
+
+    }
+
+
+    @Test
+    public void readerWriterLockCannotWriteWhileReading() throws InterruptedException {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        final ReadWriteLock lock = new ReentrantReadWriteLock();
+        final List<String> messageSequence = Collections.synchronizedList(new ArrayList<>());
+        final CountDownLatch latch = new CountDownLatch(3);
+        Runnable reader = () -> {
+
+            try {
+                lock.readLock().lock();
+                messageSequence.add("Start Reading");
+                Thread.sleep(100);
+                messageSequence.add("End Reading");
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                lock.readLock().unlock();
+                latch.countDown();
+            }
+
+        };
+
+        threadPool.submit(reader);
+        threadPool.submit(reader);
+
+        threadPool.submit(() -> {
+
+            try {
+                lock.writeLock().lock();
+                messageSequence.add("Start Writing");
+            } finally {
+                lock.writeLock().unlock();
+                latch.countDown();
+            }
+
+
+        });
+
+        latch.await();
+        assertEquals(messageSequence, Arrays.asList("Start Reading", "Start Reading", "End Reading", "End Reading", "Start Writing"));
+    }
+
+    @Test
+    public void readerWriterLockCannotReadWhileWriting() throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        final ReadWriteLock lock = new ReentrantReadWriteLock();
+        final List<String> messageSequence = Collections.synchronizedList(new ArrayList<>());
+        final CountDownLatch latch = new CountDownLatch(3);
+
+        threadPool.submit(() -> {
+
+            try {
+                lock.writeLock().lock();
+                messageSequence.add("Start Writing");
+                Thread.sleep(100);
+                messageSequence.add("End Writing");
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                lock.writeLock().unlock();
+                latch.countDown();
+            }
+        });
+
+        Runnable reader = () -> {
+
+            try {
+                lock.readLock().lock();
+                messageSequence.add("Start Reading");
+                Thread.sleep(100);
+                messageSequence.add("End Reading");
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                lock.readLock().unlock();
+                latch.countDown();
+            }
+
+        };
+
+        threadPool.submit(reader);
+        threadPool.submit(reader);
+
+        latch.await();
+        assertEquals(messageSequence, Arrays.asList("Start Writing", "End Writing", "Start Reading", "Start Reading", "End Reading", "End Reading"));
+
+    }
+
+    //resume here: https://docs.oracle.com/javase/tutorial/essential/concurrency/highlevel.html
 }
