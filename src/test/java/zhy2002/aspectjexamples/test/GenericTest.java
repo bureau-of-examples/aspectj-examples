876587ecfbaeb5eec737932f557bdf2afe38d600
zhy2002 <hz141@uowmail.edu.au>
2015-10-16 13:42:21 +1100

    Added generic test.

--- src/test/java/zhy2002/aspectjexamples/test/GenericTest.java
+++ src/test/java/zhy2002/aspectjexamples/test/GenericTest.java
@@ -2,6 +2,7 @@ package zhy2002.aspectjexamples.test;
 
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -16,11 +17,12 @@ import static org.hamcrest.Matchers.*;
 public class GenericTest {
 
     @Test
-    public void retainGenericTypeInfoTest(){
+    public void retainGenericTypeInfoTest() {
 
-        List<Integer> list = new ArrayList<Integer>(){}; //subclassing is required to retain generic info in super class
+        List<Integer> list = new ArrayList<Integer>() {
+        }; //subclassing is required to retain generic info in super class
 
-        ParameterizedType genericSuperClass = ((ParameterizedType)list.getClass().getGenericSuperclass());
+        ParameterizedType genericSuperClass = ((ParameterizedType) list.getClass().getGenericSuperclass());
 
         assertThat(genericSuperClass.getActualTypeArguments().length, equalTo(1));
 
@@ -28,6 +30,45 @@ public class GenericTest {
 
         assertThat(typeParamClass, sameInstance(Integer.class));
 
+    }
+
+    private static class MyClass {
+
+        private List<String> myListField;
+
+        public List<String> getMyListField() {
+            return myListField;
+        }
+
+        public void setMyListField(List<String> myListField) {
+            this.myListField = myListField;
+        }
+    }
+
+    /**
+     * This is how hibernate finds the generic type of a field.
+     */
+    @Test
+    public void recoverFieldGenericTypeTest() {
+
+        MyClass myObject = new MyClass();
+
+        Field[] fields = myObject.getClass().getDeclaredFields();
+
+        assertThat(fields.length, equalTo(1));
+
+        Type fieldType = fields[0].getGenericType();
+
+        assertThat(fieldType, instanceOf(ParameterizedType.class));
+
+        ParameterizedType parameterizedFieldType = (ParameterizedType)fieldType;
+
+        assertThat(parameterizedFieldType.getActualTypeArguments().length, equalTo(1));
+
+        assertThat(parameterizedFieldType.getActualTypeArguments()[0], sameInstance(String.class));
+
+        //examine the generic signature is also possible: myObject.getClass().getMethod("getMyListField").getGenericSignature()
+        //I think the rule here is type information which is available at compile time could be preserved by the compiler as metadata.
     }
 
 }
