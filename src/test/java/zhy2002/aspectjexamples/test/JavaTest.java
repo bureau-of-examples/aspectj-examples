1817407d639bb4233474a934415d10e6a20f35fa
zhy2002 <hz141@uowmail.edu.au>
2015-11-01 08:35:37 +1100

    Added weak reference tests.

--- src/test/java/zhy2002/aspectjexamples/test/JavaTest.java
+++ src/test/java/zhy2002/aspectjexamples/test/JavaTest.java
@@ -2,7 +2,11 @@ package zhy2002.aspectjexamples.test;
 
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.*;
+import java.lang.management.ManagementFactory;
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 /**
@@ -14,6 +18,38 @@ public class JavaTest {
     public void canDefineAnotherPackageClassInSameFile() {
         TopLevelPackageLevelClass topLevelPackageLevelClass = new TopLevelPackageLevelClass();//defined in another java file
         assertThat(topLevelPackageLevelClass, instanceOf(Object.class));
+    }
+
+    @Test
+    public void weakReferenceIsClearedAfterGC(){
+        Object obj  = new Object();
+        WeakReference<Object> objectWeakReference = new WeakReference<>(obj);
+        assertThat(obj, sameInstance(objectWeakReference.get()));
+
+        System.gc();
+        assertThat(obj, sameInstance(objectWeakReference.get())); //still has the strong reference
+
+        obj = null;
+        System.gc();
+        assertThat(objectWeakReference.get(), nullValue());
+
+    }
+
+    @Test
+    public void canGetJvmMxBeans(){
+        long totalCompilationTime = ManagementFactory.getCompilationMXBean().getTotalCompilationTime();
+        assertThat(totalCompilationTime, greaterThan(0L));
+    }
+
+    @Test
+    public void weakHashMapEntryIsRemovedWhenKeyObjectIsCollected(){
+        WeakHashMap<Object, String> weakHashMap = new WeakHashMap<>();
+        Object obj = new Object();
+        weakHashMap.put(obj, "test1");
+        assertThat(weakHashMap.size(), equalTo(1));
+        obj = null;
+        System.gc();
+        assertThat(weakHashMap.size(), equalTo(0));
     }
 
 }
