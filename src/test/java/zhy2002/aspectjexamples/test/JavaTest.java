38a907d32f878a1e19bbd5bd9d6b9f77c1358fa6
zhy2002 <hz141@uowmail.edu.au>
2015-12-14 09:40:42 +1100

    Added print fibo.

--- src/test/java/zhy2002/aspectjexamples/test/JavaTest.java
+++ src/test/java/zhy2002/aspectjexamples/test/JavaTest.java
@@ -21,8 +21,8 @@ public class JavaTest {
     }
 
     @Test
-    public void weakReferenceIsClearedAfterGC(){
-        Object obj  = new Object();
+    public void weakReferenceIsClearedAfterGC() {
+        Object obj = new Object();
         WeakReference<Object> objectWeakReference = new WeakReference<>(obj);
         assertThat(obj, sameInstance(objectWeakReference.get()));
 
@@ -36,13 +36,13 @@ public class JavaTest {
     }
 
     @Test
-    public void canGetJvmMxBeans(){
+    public void canGetJvmMxBeans() {
         long totalCompilationTime = ManagementFactory.getCompilationMXBean().getTotalCompilationTime();
         assertThat(totalCompilationTime, greaterThan(0L));
     }
 
     @Test
-    public void weakHashMapEntryIsRemovedWhenKeyObjectIsCollected(){
+    public void weakHashMapEntryIsRemovedWhenKeyObjectIsCollected() {
         WeakHashMap<Object, String> weakHashMap = new WeakHashMap<>();
         Object obj = new Object();
         weakHashMap.put(obj, "test1");
@@ -54,14 +54,14 @@ public class JavaTest {
 
 
     @Test
-    public void multipleBooleanTrueOrFalseInstancesPossible(){
+    public void multipleBooleanTrueOrFalseInstancesPossible() {
 
         boolean result = new Boolean(null) == Boolean.FALSE;
         assertThat(result, equalTo(false));
     }
 
     @Test
-    public void fieldWithSameNameIsHidden(){
+    public void fieldWithSameNameIsHidden() {
 
         C2 c2 = new C2();
 
@@ -72,21 +72,69 @@ public class JavaTest {
 
     }
 
+    @Test
+    public void printFiboTest() {
+        printFibo2(0);
+        printFibo2(1);
+        printFibo2(2);
+        printFibo2(3);
+        printFibo2(4);
+        printFibo2(5);
+    }
+
+
+    public void printFibo(int count) {
+        if (count <= 0) {
+            System.out.println();
+            return;
+        }
+
+        if (count == 1) {
+            System.out.println("1");
+            return;
+        }
+
+        System.out.print("1 1");
+        count -= 2;
+        int first = 1, second = 1;
+        for (int i = 0; i < count; i++) {
+            int third = first + second;
+            System.out.print(" ");
+            System.out.print(third);
+            first = second;
+            second = third;
+        }
+        System.out.println();
+    }
+
+    public void printFibo2(int count){
+        int previous = 0;
+        int current = 1;
+
+        for(int i=0; i<count; i++){
+            System.out.println(current);
+            int next = previous + current;
+            previous = current;
+            current = next;
+        }
+        System.out.println();
+    }
+
 
 }
 
 class C1 {
     int val = 10;
 
-    private int f1(){
+    private int f1() {
         return 0;
     }
 }
 
-class C2 extends C1{
+class C2 extends C1 {
     int val = 20;
 
-    protected String f1(){
+    protected String f1() {
         return "a";
     }
 }
