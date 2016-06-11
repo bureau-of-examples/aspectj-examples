686a482eed3c51d123c7a2d01d0bedf98db3ab8b
zhy2002 <hz141@uowmail.edu.au>
2016-06-11 13:22:46 +1000

    added nashorn test.

---
+++ src/test/java/zhy2002/aspectjexamples/test/ScriptEngineTest.java
@@ -0,0 +1,30 @@
+package zhy2002.aspectjexamples.test;
+
+import org.junit.Test;
+import zhy2002.aspectjexamples.domain.Member;
+
+import javax.script.*;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Test script engine behaviour.
+ */
+public class ScriptEngineTest {
+
+    /**
+     * @see https://docs.oracle.com/javase/8/docs/technotes/guides/scripting/nashorn/api.html.
+     * @throws ScriptException
+     */
+    @Test
+    public void canCreateInstanceOfLoadedClass() throws ScriptException {
+
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("nashorn");
+        Member member = (Member) scriptEngine.eval("new Packages.zhy2002.aspectjexamples.domain.Member('Jack', 32)");
+
+        assertThat(member.getName(), equalTo("Jack"));
+        assertThat(member.getAge(), equalTo(32));
+    }
+}
